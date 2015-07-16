67da97ff8783cc561a771b9a519f3265ee80ce88
Bryant Lin <blin@guidewire.com>
2015-07-16 15:52:40 -0700

    Added calendar selection for date and checks for valid input before adding task.

--- app/src/main/java/com/guidewire/microtodolist/AddTask.java
+++ app/src/main/java/com/guidewire/microtodolist/AddTask.java
@@ -1,22 +1,36 @@
 package com.guidewire.microtodolist;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.util.Log;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 @SuppressWarnings("deprecation")
 public class AddTask extends ActionBarActivity {
+    Calendar hardFinishDateCal = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener(){
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            hardFinishDateCal.set(Calendar.YEAR, year);
+            hardFinishDateCal.set(Calendar.MONTH, month);
+            hardFinishDateCal.set(Calendar.DAY_OF_MONTH, day);
+            updateLabel();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,14 @@ public class AddTask extends ActionBarActivity {
                 R.array.time_unit_options, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        EditText hardFinishDateView = (EditText)findViewById(R.id.hardFinishDate);
+        hardFinishDateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDate(v);
+            }
+        });
     }
 
     @Override
@@ -52,18 +74,106 @@ public class AddTask extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void setDate(View view) {
+        new DatePickerDialog(this, date, hardFinishDateCal.get(Calendar.YEAR),
+                hardFinishDateCal.get(Calendar.MONTH),
+                hardFinishDateCal.get(Calendar.DAY_OF_MONTH)).show();
 
+    }
+
+    private void updateLabel() {
+        String format = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
+        EditText hardFinishDateView = (EditText)findViewById(R.id.hardFinishDate);
+        hardFinishDateView.setText(sdf.format(hardFinishDateCal.getTime()));
+        
+    }
+
+    /**
+     * Checks if entered information is valid and sends back to main activity to create a task
+     * @param view
+     */
     public void finishAddingTask(View view) {
-        // Add it to Main.tasks
+        boolean everythingOK = true;
         Intent intent = new Intent();
+
+        // Task name
         EditText nameView = (EditText)findViewById(R.id.taskName);
-        intent.putExtra("name", nameView.getText().toString());
+        if (nameView.getText().toString().isEmpty()) {
+            everythingOK = false;
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("Invalid task name");
+            alertDialog.setMessage("Tasks cannot be empty.");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
+        else {
+            intent.putExtra("name", nameView.getText().toString());
+        }
 
-        EditText timeView = (EditText)findViewById(R.id.time);
-        intent.putExtra("time", Double.parseDouble(timeView.getText().toString()));
+        // Task time
+        EditText timeView = (EditText) findViewById(R.id.time);
+        try {
+            // TODO: Get global POINT_VALUE, std_dev, etc
+            double POINT_VALUE = 180;
+            double amount = Double.parseDouble(timeView.getText().toString());
+            double multiplier;
+            Spinner timeUnits = (Spinner) findViewById(R.id.timeUnits);
+            switch(timeUnits.getSelectedItemPosition()) {
+                case 0:
+                    multiplier = 1;
+                    break;
+                case 1:
+                    multiplier = 60;
+                    break;
+                case 2:
+                    multiplier = POINT_VALUE;
+                    break;
+                default:
+                    multiplier = 1;
+            }
+            intent.putExtra("time", amount * multiplier);
+        }
+        catch(NumberFormatException e) {
+            everythingOK = false;
+            Log.d("NumberFormatException", "Float not accepted");
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("Invalid amount of time");
+            alertDialog.setMessage("The amount of time for the task must be a number.");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
 
-        setResult(RESULT_OK, intent);
-        finish();
+
+        // TODO: Add all entered fields
+        EditText hardFinishDateView = (EditText)findViewById(R.id.hardFinishDate);
+        if (hardFinishDateView.getText().toString().isEmpty()) {
+            Log.d("STRING_EMPTY", "hardFinishDate is empty");
+        }
+        else {
+            // Pass in date
+            intent.putExtra("year", hardFinishDateCal.get(Calendar.YEAR));
+            intent.putExtra("month", hardFinishDateCal.get(Calendar.MONTH));
+            intent.putExtra("day", hardFinishDateCal.get(Calendar.DAY_OF_MONTH));
+
+        }
+
+        if (everythingOK) {
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+
+
 
         // TODO: if pressed back, setResult to RESULT_CANCELED
     }
