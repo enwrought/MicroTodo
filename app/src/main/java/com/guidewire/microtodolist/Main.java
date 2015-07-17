607e0e2326b463ffc9a519028601e3b57ca4bb48
Bryant Lin <blin@guidewire.com>
2015-07-16 17:28:06 -0700

    Save tasks to persistent data.

--- app/src/main/java/com/guidewire/microtodolist/Main.java
+++ app/src/main/java/com/guidewire/microtodolist/Main.java
@@ -1,6 +1,7 @@
 package com.guidewire.microtodolist;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -9,7 +10,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 @SuppressWarnings("deprecation")
@@ -18,21 +22,54 @@ public class Main extends ActionBarActivity {
     protected ArrayAdapter<String> listAdapter;
     protected ArrayList<Task> tasks;
     public static final int REQUEST_NEW_TASK = 1;
+    SharedPreferences mPrefs;
+    static final Type TASK_LIST_TYPE = new TypeToken<ArrayList<Task>>(){}.getType();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPrefs = getSharedPreferences("prefFile", 0);
+
+
+        Gson gson = new Gson();
+        String json = mPrefs.getString("tasks", "");
         tasks = new ArrayList<>();
+
+
+        if (!json.equals("")) {
+            tasks = gson.fromJson(json, TASK_LIST_TYPE);
+        }
+
+
         itemTexts = new ArrayList<>();
+        /*
         itemTexts.add("Hi");
         itemTexts.add("hi again.");
+        */
+        for (Task task : tasks) {
+            itemTexts.add(task.toString());
+        }
+
         listAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,
                 itemTexts);
         ListView list = (ListView)findViewById(R.id.taskList);
         list.setAdapter(listAdapter);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(tasks, TASK_LIST_TYPE);
+        prefsEditor.putString("tasks", json);
+        prefsEditor.commit();
+    }
+
 
     public void addTask(Task task) {
         tasks.add(task);
